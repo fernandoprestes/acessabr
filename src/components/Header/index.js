0ff127fdf442584666a2ceb8b041aed7f8b1bf48
Fernando Prestes <fernandoprestesc@gmail.com>
2021-11-23 16:27:13 -0300

    create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logoAcessaBR from './../../images/logo-acessabr.svg'
+import logoEbac from './../../images/logo-ebac.svg'
+import './style.scss'
+
+export const Header = () => {
+  return (
+    <header className="header__container">
+      <div className="header__logo header__logo--state">
+        <img src={logoAcessaBR} alt="logo acessa br" />
+        <span className="header__city"> / São Paulo - SP</span>
+      </div>
+      <div className="header__logo header__logo--ebac">
+        <span className="header__span">apio: </span>
+        <img src={logoEbac} alt="logo ebac" />
+      </div>
+    </header>
+  )
+}
