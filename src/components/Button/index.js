4684888584866ba444b9df6cfcdf78c6728c658b
Fernando Prestes <fernandoprestesc@gmail.com>
2021-11-25 19:14:14 -0300

    Create button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.scss'
+import PropTypes from 'prop-types'
+
+export const Button = ({ children, onClick, width }) => {
+  return (
+    <button className="button" onClick={onClick} style={{ width }}>
+      {children}
+    </button>
+  )
+}
+
+Button.prototype = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  onClick: PropTypes.func.isRequired,
+  width: PropTypes.number.isRequired
+}
